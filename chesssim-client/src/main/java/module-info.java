8aa5af3e35791348e722a9c4f3e3d63382918c07
Rene Polak <ic24b022@technikum-wien.at>
2024-10-21 08:04:34 +0200

    Had to fix some project setup related issues otherwise the project wont start

--- chesssim-client/src/main/java/module-info.java
+++ chesssim-client/src/main/java/module-info.java
@@ -15,7 +15,7 @@ module at.chess.chesssimulator {
 
         // Open the package to reflection (if FXML needs more access, like private fields or methods)
         opens at.chess.chesssimulator.board to javafx.fxml;
-        opens pieces;
+        opens at.chess.chesssimulator.piece;
     opens at.chess.chesssimulator.controller to javafx.fxml;
     exports at.chess.chesssimulator.board.ui to javafx.fxml;
     opens at.chess.chesssimulator.board.ui to javafx.fxml;
